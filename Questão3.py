11fe935fd20be5f0efacf083767dd2d0719fefd3
JULIO CESAR STAMATI <135392044+JulioStamati@users.noreply.github.com>
2024-09-15 12:34:06 -0300

    Add files via upload

---
+++ Questão3.py
@@ -0,0 +1,12 @@
+# Inicializando as variáveis
+INDICE = 12
+SOMA = 0
+K = 1
+
+# Executando o loop enquanto K for menor que INDICE
+while K < INDICE:
+    K = K + 1
+    SOMA = SOMA + K
+
+# Imprimindo o resultado
+print(SOMA)
